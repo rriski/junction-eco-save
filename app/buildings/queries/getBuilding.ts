cfda24de62c7bb054bacd779da70f6b6056954f7
Henrik Aarnio <henrik@blok.ai>
2020-11-07 20:57:34 +0200

    include renovations

--- app/buildings/queries/getBuilding.ts
+++ app/buildings/queries/getBuilding.ts
@@ -53,7 +53,7 @@ const energy_consumption: energy_consumption_options = {
 }
 
 export default async function getBuilding({ where }: GetBuildingInput, ctx: Ctx) {
-  const building:ApiBuilding = await db.building.findFirst({ where });
+  const building:ApiBuilding = await db.building.findFirst({ where, include: { Renovation: true }});
 
   if (building?.construction_date && (building.area_living || building.area_floors)){
 
