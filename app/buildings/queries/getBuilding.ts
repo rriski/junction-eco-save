a047c01746fcec751dbabb00a5dd6a9091f16442
Henrik Aarnio <henrik@blok.ai>
2020-11-07 21:39:43 +0200

    use correct energy consumption by category

--- app/buildings/queries/getBuilding.ts
+++ app/buildings/queries/getBuilding.ts
@@ -19,6 +19,32 @@ type energy_consumption_options = {
   [key: string]: energy_data;
 };
 
+const energy_consumption_omakotitalo: energy_consumption_options = {
+  '201': {
+    heating: 116,
+    electricity: 12,
+  },
+  '200': {
+    heating: 116,
+    electricity: 12,
+  },
+  '199': {
+    heating: 172,
+    electricity: 10,
+  },
+  '198': {
+    heating: 172,
+    electricity: 10,
+  },
+  '197': {
+    heating: 228,
+    electricity: 8,
+  },
+  '196': {
+    heating: 228,
+    electricity: 8,
+  },
+}
 const energy_consumption: energy_consumption_options = {
   '201': {
     heating: 82,
@@ -61,7 +87,14 @@ export default async function getBuilding({ where }: GetBuildingInput, ctx: Ctx)
   });
 
   if (building?.construction_date && (building.area_living || building.area_floors)) {
-    const energy = energy_consumption[
+    const energy = building.category === "Omakotitalo" ?
+    energy_consumption_omakotitalo[
+      Math.floor(building.construction_date.getFullYear() / 10).toString()
+    ] || {
+      heating: 286,
+      electricity: 8,
+    }
+    : energy_consumption[
       Math.floor(building.construction_date.getFullYear() / 10).toString()
     ] || {
       heating: 130,
